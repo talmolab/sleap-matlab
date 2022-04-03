562c3291ab213750be2060869d941410aa194f7d
Vijay Iyer <viyer05@gmail.com>
2022-04-03 18:11:37 -0400

    add sleap.data.* namespace for data processing functions

---
+++ +sleap/+data/makeVideoDatastore.m
@@ -0,0 +1,21 @@
+function vds = makeVideoDatastore(videoPath)
+%makeVideoDatastore   Utility function to make a FileDatastore that incrementally reads from video files.
+%
+vds = fileDatastore(videoPath, "ReadMode", "partialfile", "ReadFcn", @incrementalVideoReadFcn);
+end
+
+function [data, reader, done] = incrementalVideoReadFcn(filename, reader)
+if isempty(reader)
+   reader = VideoReader(filename);
+end
+
+data = reader.readFrame(); 
+% data = preprocessIm(data, userdata.ResizeScale);
+done = ~reader.hasFrame();
+end
+
+% function data = preprocessIm(data, inputScale)
+% data = rgb2gray(data);
+% data = single(data) / 255;
+% data = imresize(data, inputScale);
+% end
